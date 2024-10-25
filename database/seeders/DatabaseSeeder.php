97774959202708dd01b98a87ea7d62ed48b960ce
perso <bidault.axel.antoine@gmail.com>
2024-10-25 15:28:56 +0200

    bdd fonctionnelle

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,44 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create Categories
-        $this->call(CategorySeeder::class);
-
         // Create weeks
         $this->call(WeekSeeder::class);
+        $this->call(CategorySeeder::class);
 
-//        // Create content
-//        User::factory()
-//            ->count(15)
-//            ->has(
-//                Track::factory(config('app.tracks_per_week'))
-//                    ->state(new Sequence(fn () => ['week_id' => rand(2, 7)]))
-//                    ->sample()
-//            )
-//            ->has(Code::factory(config('app.codes_count')))
-//            ->sequence(function (Sequence $sequence) {
-//                $id = str_pad($sequence->index + 1, 4, "0", STR_PAD_LEFT);
-//
-//                return ['email' => "user{$id}@example.com"];
-//            })
-//            ->create();
+        // Create content
         User::factory()
             ->count(15)
+            ->has(
+                Track::factory(config('app.tracks_per_week'))
+                    ->state(new Sequence(fn () => ['week_id' => rand(2, 7)]))
+                    ->sample()
+            )
+            ->has(Code::factory(config('app.codes_count')))
             ->sequence(function (Sequence $sequence) {
-                // Crée un email unique en utilisant l'index de la séquence
                 $id = str_pad($sequence->index + 1, 4, "0", STR_PAD_LEFT);
-                $email = "user{$id}@example.com";
 
-                // Vérifie si l'email existe déjà, sinon crée un nouvel utilisateur
-                return [
-                    'email' => $email,
-                    'password' => bcrypt('password'), // Assure-toi d'avoir un mot de passe
-                ];
+                return ['email' => "user{$id}@example.com"];
             })
             ->create();
-
-
-
     }
 
 
